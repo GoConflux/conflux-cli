1cde8fd5af63b4c1ef742e7ef5069de1c28b8c71
Ben Whittle <benwhittle31@gmail.com>
2016-05-22 22:57:45 -0700

    removing railtie and logic that should go in the ruby gem

--- spec/conflux_spec.rb
+++ spec/conflux_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 describe Conflux do
 
   it 'has a version number' do
-  expect(Conflux::VERSION).not_to be nil
-  end
-
-  it 'has a generator' do
-    generator = Conflux::Generators::ConfluxGenerator.new
-    expect(generator).not_to be nil
+    expect(Conflux::VERSION).not_to be nil
   end
 
 end
