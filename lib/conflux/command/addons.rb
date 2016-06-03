bad3c42d60cf3ac1d84ede4a4b03ee739d6fc3c4
Ben Whittle <benwhittle31@gmail.com>
2016-06-03 03:01:39 -0700

    added addons:add command

--- lib/conflux/command/addons.rb
+++ lib/conflux/command/addons.rb
@@ -1,5 +1,6 @@
 require 'conflux/command/abstract_command'
 require_relative '../api/addons'
+require_relative '../pull'
 
 class Conflux::Command::Addons < Conflux::Command::AbstractCommand
 
@@ -13,7 +14,52 @@ class Conflux::Command::Addons < Conflux::Command::AbstractCommand
   end
 
   def add
-    display 'heard conflux addons:add'
+    if ![1, 3].include?(@args.length) || (@args.length == 3 && @args[1] != '-a')
+      # return command help
+      return
+    end
+
+    addon_slug, plan = @args.first.split(':')
+    app_slug = @args[2]
+
+    if app_slug.nil?
+      if File.exists?(conflux_manifest_path)
+        manifest = JSON.parse(File.read(conflux_manifest_path)) rescue {}
+
+        @manifest_creds = manifest['configs'] || {}
+
+        if !@manifest_creds.key?('CONFLUX_USER') || !@manifest_creds.key?('CONFLUX_APP')
+          reply_no_conflux_app
+          return
+        end
+
+        headers = {
+          'Conflux-User' => @manifest_creds['CONFLUX_USER'],
+          'Conflux-App' => @manifest_creds['CONFLUX_APP']
+        }
+      else
+        reply_no_conflux_app
+      end
+    else
+      ensure_authed
+      headers = { 'Conflux-User' => @password }
+    end
+
+    data = {
+      app_slug: app_slug,
+      addon_slug: addon_slug,
+      plan: plan
+    }
+
+    RestClient.post("#{host_url}/app_addons", data, headers) do |response|
+      if response.code == 200
+        body = JSON.parse(response.body) rescue {}
+        display "Successfully added #{addon_slug} to #{body['app_slug'] || app_slug}."
+        Conflux::Pull.perform
+      else
+        error "Error adding #{addon_slug} as an addon."
+      end
+    end
   end
 
   #----------------------------------------------------------------------------
@@ -32,7 +78,6 @@ class Conflux::Command::Addons < Conflux::Command::AbstractCommand
 
     module Add
       DESCRIPTION = 'Adds and addon to a conflux app'
-      VALID_ARGS = {}
     end
 
   end
