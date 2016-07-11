076f8c9f190ffaeccd1041ff5d6e203939dec713
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-07-11 15:13:18 -0700

    modifying description of apps:current

--- lib/conflux/command/apps.rb
+++ lib/conflux/command/apps.rb
@@ -86,7 +86,7 @@ class Conflux::Command::Apps < Conflux::Command::AbstractCommand
     end
 
     module Current
-      DESCRIPTION = 'Shows which conflux app is connected to the current directory'
+      DESCRIPTION = 'Show which conflux app is connected to the current directory'
       VALID_ARGS = [ [] ]
     end
 
